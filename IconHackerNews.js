7b00e2b084b76cf2a5a1980c97d4f2d51b5bd8f6
Dennis van Oosterhout <dennis.van.oosterhout@nl.pwc.com>
2015-04-01 16:43:42 +0200

    Improved usage of CSS above HTML attributes.

--- IconHackerNews.js
+++ IconHackerNews.js
@@ -1,7 +1,7 @@
 var IconHackerNews = React.createClass({
   render: function() {
     // I got the source SVG frome somewhere on the web.
-    return (<svg width="16px" height="16px" viewBox="0 0 512 512">
+    return (<svg viewBox="0 0 512 512" style={{width:16, height:16}}>
               <path d="M416,96v320H96V96H416 M448,64H64v384h384V64L448,64z" fill="#000000"/>
               <path d="M296.7,159H342l-63.9,120v72h-39.9v-72L172,159h47.1l39.7,83.6L296.7,159z" fill="#000000"/>
             </svg>);
