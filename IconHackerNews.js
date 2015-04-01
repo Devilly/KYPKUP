c959099d14d2e39fcb7375db44b021b80f861426
Dennis van Oosterhout <dennis.van.oosterhout@nl.pwc.com>
2015-04-01 16:33:20 +0200

    Removed unnecessary grouping.

--- IconHackerNews.js
+++ IconHackerNews.js
@@ -1,12 +1,8 @@
 var IconHackerNews = React.createClass({
   render: function() {
     return (<svg x="0px" y="0px" width="16px" height="16px" viewBox="0 0 512 512">
-              <g>
-                <path d="M416,96v320H96V96H416 M448,64H64v384h384V64L448,64z" fill="#000000"/>
-                <g>
-                  <path d="M296.7,159H342l-63.9,120v72h-39.9v-72L172,159h47.1l39.7,83.6L296.7,159z" fill="#000000"/>
-                </g>
-              </g>
+              <path d="M416,96v320H96V96H416 M448,64H64v384h384V64L448,64z" fill="#000000"/>
+              <path d="M296.7,159H342l-63.9,120v72h-39.9v-72L172,159h47.1l39.7,83.6L296.7,159z" fill="#000000"/>
             </svg>);
   }
 });
