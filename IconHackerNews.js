12fe723da405c51b16b3a29632715f8ed835e567
Dennis van Oosterhout <dennis.van.oosterhout@nl.pwc.com>
2015-04-02 14:02:03 +0200

    Improved the transform of the icons.

--- IconHackerNews.js
+++ IconHackerNews.js
@@ -2,7 +2,7 @@ var IconHackerNews = React.createClass({
   render: function() {
     // Path taken from Ionicons' ion-social-hackernews-outline.
     return (<svg viewBox="0 0 400 400" style={{width:16, height:16}}>
-              <path transform="rotate(180 200 200)" d="M352 352h-320v-320h320v320zM384 384v0v-384h-384v384h384zM233 289h45l-64 -120v-72h-40v72l-66 120h47l40 -84z" />
+              <path transform="scale(1,-1) translate(0,-400)" d="M352 352h-320v-320h320v320zM384 384v0v-384h-384v384h384zM233 289h45l-64 -120v-72h-40v72l-66 120h47l40 -84z" />
             </svg>);
   }
 });
