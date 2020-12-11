9d816428c1e22748674d59cad465023423f1ddd4
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2020-12-11 19:51:07 +0100

    Use indentation for all but first line.

--- src/Entry.js
+++ src/Entry.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 export default props => {
+  var linkStyle = {
+    display: 'block',
+    marginLeft: '1em',
+    textIndent: '-1em'
+  };
+
   return (
     <li>
-      <a href={props.url}>{props.title}</a>
+      <a style={linkStyle} href={props.url}>{props.title}</a>
     </li>
   );
 };
